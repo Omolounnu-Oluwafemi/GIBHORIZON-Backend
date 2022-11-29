78833645c34c8bc2403d378893a4d1e3834fa61c
Oluwafemi <oluwafemiomolounnu@gmail.com>
2022-11-29 16:23:28 +0100

    finished with login and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,104 @@
 import {db} from "../db.js"
+import { createTransport } from "nodemailer";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+
 
 export const signup = (req, res)=>{
 
-    //CHECKING EXISTING USER
-    const q = "SELECT * FROM users WHERE email = ? OR firstname = ?"
+    // Create the password 
+    const password = ("GIB" + Math.random().toString(36).slice(2));
 
-    db.query(q,[req.body.email, req.body.name, req.body.name, req.body.phone], (err, data)=>{
-        if(err) return res.json(err)
+
+    //CHECKING EXISTING USER    
+    const q = "SELECT * FROM users WHERE email = ?";
+
+    db.query(q, [req.body.email], (err, data)=>{
+
+        if(err) return res.status(500).json(err);
         if(data.length) return res.status(409).json("User already exists!");
 
-        //create a User
+        const p = "INSERT INTO users(firstname, lastname, email, phone, password) VALUES (?)";
 
-        const q = "INSERT INTO users(firstname, lastname, email, phone, password) VALUES (?)"
         const values = [
             req.body.firstname,
             req.body.lastname,
             req.body.email,
             req.body.phone,
-            "GIB" + Math.random().toString(36).slice(2)
-        ]
+            password
+        ];
 
-        db.query(q, [values], (err,data)=>{
-            if(err) return res.json(err);
+        db.query(p, [values], (err,data)=>{
+            if (err) return res.status(500).json(err);
             return res.status(200).json("User has been created")
-        })
-    }) 
-}
+        });
+    });
+   
+    //Send password to user's email
+const transporter = createTransport({
+    host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: "oluwafemiomolounnu@gmail.com",
+    pass: "adumsdmnnozmzoal"
+    }
+});
+
+ const message = {
+    from: "Oluwafemi",
+    to: req.body.email,
+    subject: "Your Password",
+    text: "Your password is " + password
+};
+
+transporter.sendMail(message, function(err, info) {
+        if (err){
+    console.log(err)
+        } else
+        console.log( "sent:" + info);
+}); 
+
+};
+
+
+
+
+export const login = (req, res)=>{
+
+    //CHECK USER
+
+    const q = "SELECT * FROM users WHERE email = ?"
+
+    db.query(q, [req.body.email,], (err, data)=> {
+        if(err) return res.json(err)
+        if(data.length === 0) 
+        return res.status(404).json("User not found!");
+ 
+        //CHECK PASSWORD
+        const isPasswordCorrect =
+            req.body.password;
+
+            // if (isPasswordCorrect) {
+            //     return res.send('Sign In successfully');
+            // }
+        if (!isPasswordCorrect) 
+            return res.status(400).json("Wrong Email or Password!");
+      
+const token = jwt.sign({id:data[0].id}, "jwtkey");
+const {password, ...other}= data[0]
+
+res.cookie("access_token", token, {
+httpOnly:true,
+})
+.status(200)
+.json(other);
+    });
+};
+
+export const logout = (req, res) => {
+    res.clearCookie("access_token",{
+      sameSite:"none",
+      secure:true
+    }).status(200).json("User has been logged out.")
+  };
--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import express from "express"
 import authRoutes from "./routes/auth.js"
 import tableRoutes from "./routes/tables.js"
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import session from "express-session";
+import cors from "cors";
 
-const app = express()   
 
-app.use(express.json())
+const app = express(); 
+
+app.use(express.json());
+// app.use(cors({
+//     origin: ["http://localhost:3000"],
+//     methods: ["GET", "POST"],
+//     credentials: true
+// }));
+app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(session({
+//     key: "userId",
+//     secret: "subscribe",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         expires: 60 * 60 * 24,
+//     },
+// }))
+
+// app.get("/login", (req, res)=>{
+// if(req.session.user){
+//     res.send({loggedIn: true, user: req.session.user})
+// }   else{
+//     res.send({loggedIn: false})
+// }
+// });
 
 app.use("/api/auth", authRoutes)
-app.use("/api/tables", tableRoutes);
+app.use("/api/tables", tableRoutes); 
 
 app.listen(8800, ()=>{
 console.log ("connected!")
